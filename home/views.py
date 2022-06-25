502375a7195a64bbb353c47be62f6b6d79a87e5f
theanxiuser <bishalpoudel607@gmail.com>
2022-06-25 11:06:08 +0545

    indexview

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views import View
+# from django.contrib import messages
 
 # Create your views here.
+class IndexView(View):
+    """Return index.html if not logged in, else redirect to feed"""
+    def get(self, request):
+        if request.user.is_authenticated:
+            # messages.info(request, "You are already logged in")
+            return redirect(reverse_lazy('mynw:feed'))
+        return render(request, "home/index.html")
