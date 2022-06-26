e5ffcfeedad2d76ec982904eb7b6fd402efdb0c3
theanxiuser <bishalpoudel607@gmail.com>
2022-06-26 18:24:42 +0545

    ckeditor added

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from .models import Post
+from ckeditor.widgets import CKEditorWidget
+from django.db import models
 
 # Register your models here.
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "status")
+    list_filter = ("status",)
+    search_fields = ("title", "content")
+    formfield_overrides = {
+        models.TextField: {'widget':
+                               CKEditorWidget()}
+    }
 
 admin.site.register(Post, PostAdmin)
